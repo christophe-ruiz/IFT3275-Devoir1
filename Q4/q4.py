c109a90116a41991ba8e38ac14f5c3e068b938f5
Christophe Ruiz <christophe.ruiz@etu.univ-amu.fr>
2022-03-12 13:31:13 -0500

    q4 implemented

--- Q4/q4.py
+++ Q4/q4.py
@@ -1,13 +1,56 @@
+import re
+
+cipher = []
+
+
 # Filter given input in question 4
-# lines = []
-# if __name__ == '__main__':
-#     with open('input4.txt', 'r', encoding="utf-8") as f:
-#         lines = [line.strip("\n") for line in f if line != "\n"]
-#         with open('output4.txt', 'w', encoding="utf-8") as o:
-#             o.write("".join(lines))
+def sanitize():
+    with open('input4.txt', 'r', encoding="utf-8") as rawfile:
+        lines = [line.strip("\n") for line in rawfile if line != "\n"]
+        with open('output4.txt', 'w', encoding="utf-8") as o:
+            o.write("".join(lines))
+
+
+def str_to_binary(raw):
+    res = ""
+    for c in raw:
+        res += format(ord(c), '#010b')[2:]
+    n = int(res, 2)
+    return n
+
+
+def binary_to_str(raw):
+    res = ""
+    for i in range(0, len(raw), 8):
+        res += chr(int(raw[i:i+8], 2))
+    return res
+
+
+def each_cons(xs, n):
+    return zip(*(xs[i:] for i in range(n)))
+
 
 if __name__ == '__main__':
-    raw = ""
+    regex = re.compile("[a-zA-Z'éùûèàÀÉÊêëË\-\.,!\?;:\ ]*")
     with open('output4.txt', 'r', encoding='utf-8') as f:
-        raw = f.readline()
-    print(f'--RAW:{raw}')
+        cipher = f.readline()
+    print(cipher)
+
+    xor = int(cipher[:len(cipher)//2], 2) ^ int(cipher[len(cipher)//2:], 2)
+    xor = "0" + bin(xor)[2:]
+    print(xor)
+    while True:
+        word = input("Choisissez le mot: ")
+        print(f'word: {word}')
+        longueur = 8 * len(word)
+        print(f'longueur {longueur}')
+        word = str_to_binary(word)
+        print(f'binary word: {word}')
+
+        for i in range(len(xor)-longueur):
+            # print(cipher[i:i+longueur])
+            res = int(cipher[i:i+longueur], 2) ^ word
+            strres = format(res, f'0{longueur}b')
+            clearword = binary_to_str(strres)
+            if regex.fullmatch(clearword):
+                print(clearword)
